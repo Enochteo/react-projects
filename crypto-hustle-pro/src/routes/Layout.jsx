4e773c8bb56652cec04a29b45944dc70f0f22773
Enochteo <enochowoade@gmail.com>
2025-08-21 01:38:47 -0500

    add routes

--- crypto-hustle-pro/src/routes/Layout.jsx
+++ crypto-hustle-pro/src/routes/Layout.jsx
@@ -7,7 +7,7 @@ function Layout(){
             <nav>
                 <ul>
                     <li className="home-link" key='home-button'>
-                        <Link style={{ color: 'white' }} to='/'>Home</Link>
+                        <Link style={{ color: 'white' }} to='/'>Home </Link>
                     </li>
                 </ul>
             </nav>
